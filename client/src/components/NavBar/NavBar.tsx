0e38f8c4f223301e65a4674c4dd5fb2f6c14e47c
Sushant Huilgol <sushanthuilgol2114@gmail.com>
2024-05-22 05:42:16 +0530

    Added NavBar with routing and changed some basic css

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,9 +1,22 @@
 import './NavBar.scss'
+import logo from '../../assets/logo.jpg'
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+      <img src={logo} alt="" className='logo'/>
+      <p className='logoText'>PetPal</p>
+      <nav>
+        <NavLink to="/" className="li">Home</NavLink>
+        <NavLink to={'adoption'} className="li">Adoption</NavLink>
+        <NavLink to={'events'} className="li">Events</NavLink>
+        <NavLink to={'petservices'} className="li">Services</NavLink>
+        <NavLink to={'lostandfound'} className="li">Lost&Found</NavLink>
+        <NavLink to={'petfacilities'} className="li">Facilities</NavLink>
+      </nav>
+    </div>
   )
 }
-
+//Chicle
 export default NavBar
