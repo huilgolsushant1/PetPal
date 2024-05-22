1ff665825e8dcffb755959d3a6cc1185d1a56e9f
huilgolsushant1 <huilgolsushant1@gmail.com>
2024-05-22 05:42:16 +0530

    Added NavBar with routing and changed some basic css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,22 +7,24 @@ import LostAndFound from './pages/LostAndFound/LostAndFound';
 import PetFacilities from './pages/PetFacilities/PetFacilities';
 import PetServices from './pages/PetServices/PetServices';
 import Login from './pages/Login/Login';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
 
   return (
-      <div className='app'>
-      <div className="main">
-        <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/adoption" element={<Adoption />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/petfacilities" element={<PetFacilities />} />
-        <Route path="/petservices" element={<PetServices />} />
-        <Route path="/lostandfound" element={<LostAndFound />} />
-        <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
+      <div className='container'>
+        <NavBar></NavBar>
+        <div className="main">
+          <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/adoption" element={<Adoption />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/petfacilities" element={<PetFacilities />} />
+          <Route path="/petservices" element={<PetServices />} />
+          <Route path="/lostandfound" element={<LostAndFound />} />
+          <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
       </div>
      
   )
